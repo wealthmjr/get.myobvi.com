bd71704913d9761eddda133a7750afcc85b8e041
Cory Stevenson <cory@lab270.com>
2020-08-31 16:45:40 -0500

    init

--- routes/web.php
+++ routes/web.php
@@ -19,80 +19,112 @@ Route::get('/', function () {
 
 Route::group(['prefix' => '/api'], function () {
 
-    Route::group(['prefix' => '/facebook'], function () {
+    Route::group(['prefix' => '/inbound'], function () {
 
-        Route::any('/postback', function (\Illuminate\Http\Request $request) {
+        Route::group(['prefix' => '/shopify'], function () {
 
-            $pixelId = $request->get('pixel_id');
+            Route::any('/webhooks/create-checkout', function (\Illuminate\Http\Request $request) {
 
-            if ($pixelId === null)
-            {
-                exit('no pixel id detected');
-            }
+                $client = new \GuzzleHttp\Client();
+                $client->post('https://webhook.site/351b111d-5759-4b54-ac69-3a7aafd03ad2', [
+                   'json' => $request->getContent()
+                ]);
 
-            $testCode = $request->get('test_code', null);
+            });
 
-            $payout = $request->get('price', 0);
-            $currency = 'USD';
+            Route::any('/webhooks/create-order', function (\Illuminate\Http\Request $request) {
 
-            $fbClid = $request->get('fbclid', null);
-            $fbc = $request->get('fbc', null);
-            $fbp = $request->get('fbp', null);
+                $client = new \GuzzleHttp\Client();
+                $client->post('https://webhook.site/351b111d-5759-4b54-ac69-3a7aafd03ad2', [
+                    'json' => $request->getContent()
+                ]);
 
-            $ipAddress = $request->get('ip_address', '127.0.0.1');
-            $userAgent = $request->get('user_agent', null);
-
-            $eventName = $request->get('event_name', 'Purchase');
-            $eventTime = $request->get('event_time', time());
-            $eventId = $request->get('event_id', time());
-
-            $emailAddress = $request->get('email_address', null);
-            $phoneNumber = $request->get('phone_number', null);
-
-            $city = $request->get('city', null);
-            $state = $request->get('state', null);
-            $zipCode = $request->get('zip_code', null);
-            $countryCode = $request->get('country_code', null);
-
-            $firstName = $request->get('first_name', null);
-            $lastName = $request->get('last_name', null);
-            $gender = $request->get('gender', null);
-            $dob = $request->get('dob', null);
-
-            $emailAddressHashed = hash('sha256', $emailAddress);
-            $phoneNumberHashed = hash('sha256', $phoneNumber);
-
-            $userData = [
-                'email' => $emailAddress,
-                'phone' => $phoneNumber,
-                'ip_address' => $ipAddress,
-                'user_agent' => $userAgent,
-                'city' => $city,
-                'state' => $state,
-                'zip_code' => $zipCode,
-                'country_code' => $countryCode,
-                'first_name' => $firstName,
-                'last_name' => $lastName,
-                'gender' => $gender,
-                'dob' => $dob,
-                'fbp' => $fbp,
-                'fbc' => $fbc,
-                'fbclid' => $fbClid,
-            ];
-
-            $customData = [
-                'currency' => $currency,
-                'value' => $payout
-            ];
-
-            $fbAPI = new \App\Http\API\Facebook('EAADTiZCMJzzkBAK9dJFH6TzioMA2rTNZBzsxA3uMbjrNKwjRuJHTR6zxBHWVPlgJlCgVVSWgPRPhhqpOjmSmnFvrAu5ldHY77ddigac7E73ZCwMvdZCPqxmqfKnaY0shAm0SjeSmjZC0uosmbeEd6VC3zq4Y6v8uIMZBJigzWD8lPffWZCrZBlZBnwN1chRkpMqgZD');
-            $response = $fbAPI->sendServerSideEvent($pixelId, $eventName, $eventTime, $eventId, $userData, $customData, $testCode);
-
-            return $response;
-
+            });
 
         });
 
     });
+
+    Route::group(['prefix' => '/outbound'], function () {
+
+        Route::group(['prefix' => '/facebook'], function () {
+
+            Route::any('/postback', function (\Illuminate\Http\Request $request) {
+
+                $pixelId = $request->get('pixel_id');
+
+                if ($pixelId === null)
+                {
+                    exit('no pixel id detected');
+                }
+
+                $testCode = $request->get('test_code', null);
+
+                $payout = $request->get('price', 0);
+                $currency = 'USD';
+
+                $fbClid = $request->get('fbclid', null);
+                $fbc = $request->get('fbc', null);
+                $fbp = $request->get('fbp', null);
+
+                $ipAddress = $request->get('ip_address', '127.0.0.1');
+                $userAgent = $request->get('user_agent', null);
+
+                $eventName = $request->get('event_name', 'Purchase');
+                $eventTime = $request->get('event_time', time());
+                $eventId = $request->get('event_id', time());
+
+                $emailAddress = $request->get('email_address', null);
+                $phoneNumber = $request->get('phone_number', null);
+
+                $city = $request->get('city', null);
+                $state = $request->get('state', null);
+                $zipCode = $request->get('zip_code', null);
+                $countryCode = $request->get('country_code', null);
+
+                $firstName = $request->get('first_name', null);
+                $lastName = $request->get('last_name', null);
+                $gender = $request->get('gender', null);
+                $dob = $request->get('dob', null);
+
+                $emailAddressHashed = hash('sha256', $emailAddress);
+                $phoneNumberHashed = hash('sha256', $phoneNumber);
+
+                $userData = [
+                    'email' => $emailAddress,
+                    'phone' => $phoneNumber,
+                    'ip_address' => $ipAddress,
+                    'user_agent' => $userAgent,
+                    'city' => $city,
+                    'state' => $state,
+                    'zip_code' => $zipCode,
+                    'country_code' => $countryCode,
+                    'first_name' => $firstName,
+                    'last_name' => $lastName,
+                    'gender' => $gender,
+                    'dob' => $dob,
+                    'fbp' => $fbp,
+                    'fbc' => $fbc,
+                    'fbclid' => $fbClid,
+                ];
+
+                $customData = [
+                    'currency' => $currency,
+                    'value' => $payout
+                ];
+
+                $fbAPI = new \App\Http\API\Facebook('EAADTiZCMJzzkBAK9dJFH6TzioMA2rTNZBzsxA3uMbjrNKwjRuJHTR6zxBHWVPlgJlCgVVSWgPRPhhqpOjmSmnFvrAu5ldHY77ddigac7E73ZCwMvdZCPqxmqfKnaY0shAm0SjeSmjZC0uosmbeEd6VC3zq4Y6v8uIMZBJigzWD8lPffWZCrZBlZBnwN1chRkpMqgZD');
+                $response = $fbAPI->sendServerSideEvent($pixelId, $eventName, $eventTime, $eventId, $userData, $customData, $testCode);
+
+                return $response;
+
+
+            });
+
+        });
+
+    });
+
+
 
 });
