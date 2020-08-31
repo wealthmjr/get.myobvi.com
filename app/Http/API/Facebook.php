c5d5a61d1fb402f315695157bad7e2b23953a223
Cory Stevenson <cory@lab270.com>
2020-08-31 17:53:42 -0500

    init

--- app/Http/API/Facebook.php
+++ app/Http/API/Facebook.php
@@ -9,11 +9,13 @@ use FacebookAds\Object\ServerSide\CustomData;
 use FacebookAds\Object\ServerSide\Event;
 use FacebookAds\Object\ServerSide\EventRequest;
 use FacebookAds\Object\ServerSide\UserData;
+use GuzzleHttp\Client;
 
 class Facebook
 {
 
     private $accessToken;
+    private $baseApiUrl = 'https://graph.facebook.com/v8.0';
 
     public function __construct($accessToken=null)
     {
@@ -164,6 +166,149 @@ class Facebook
 
 
         dd($response);
+
+        return $response;
+
+    }
+
+    public function sendServerSideEventCustom($pixelId=null, $eventName='', $eventTime=0, $eventId=null, $userDataArr=[], $customDataArr=[], $testCode=null)
+    {
+
+        $access_token = $this->accessToken;
+        $pixel_id = $pixelId;
+
+        if($access_token === null || $pixelId === null)
+        {
+            exit('required fields missing');
+        }
+
+        $user_data = [];
+
+        if(isset($userDataArr['ip_address']))
+        {
+            $user_data[] = ($userDataArr['ip_address']);
+        }
+
+        if(isset($userDataArr['user_agent']))
+        {
+            $user_data[] = ($userDataArr['user_agent']);
+        }
+
+        if (isset($userDataArr['email']))
+        {
+            $user_data[] = ($userDataArr['email']);
+        }
+
+        if (isset($userDataArr['phone']))
+        {
+            $user_data[] = ($userDataArr['phone']);
+        }
+
+        if (isset($userDataArr['fbc']))
+        {
+            $user_data[] = ($userDataArr['fbc']);
+        }
+
+        if (isset($userDataArr['fbclid']))
+        {
+            $fbc = 'fb.1.' . time() . '.' . $userDataArr['fbclid'];
+            $user_data[] = ($fbc);
+        }
+
+        if (isset($userDataArr['fbp']))
+        {
+            $user_data[] = ($userDataArr['fbp']);
+        }
+
+        if (isset($userDataArr['first_name']))
+        {
+            $user_data[] = ($userDataArr['first_name']);
+        }
+
+        if (isset($userDataArr['last_name']))
+        {
+            $user_data[] = ($userDataArr['last_name']);
+        }
+
+        if (isset($userDataArr['city']))
+        {
+            $user_data[] = ($userDataArr['city']);
+        }
+
+        if (isset($userDataArr['state']))
+        {
+            $user_data[] = ($userDataArr['state']);
+        }
+
+        if (isset($userDataArr['zip_code']))
+        {
+            $user_data[] = ($userDataArr['zip_code']);
+        }
+
+        if (isset($userDataArr['country_code']))
+        {
+            $user_data[] = ($userDataArr['country_code']);
+        }
+
+        if (isset($userDataArr['gender']))
+        {
+            $user_data[] = ($userDataArr['gender']);
+        }
+
+        if (isset($userDataArr['dob']))
+        {
+            $user_data[] = ($userDataArr['dob']);
+        }
+
+        $custom_data = [];
+
+        if (isset($customDataArr['currency']))
+        {
+            $custom_data[] = ($customDataArr['currency']);
+        }
+
+        if (isset($customDataArr['value']))
+        {
+            $custom_data[] = ($customDataArr['value']);
+        }
+
+        $event = [];
+
+        if($eventId !== null)
+        {
+            $event[] = ($eventId);
+        }
+
+        if (isset($customDataArr['value']))
+        {
+            $custom_data[] = ($customDataArr['value']);
+        }
+
+        $events = array();
+        array_push($events, $event);
+
+        $request = [
+            'data' => [
+                'event_name' => $eventName,
+                'event_time' => $eventTime,
+                'event_id' => $eventId,
+                'user_data' => $user_data,
+                'custom_data' => $custom_data
+            ]
+        ];
+
+        dd(json_encode($request));
+
+        if($testCode !== null)
+        {
+            $request['test_event_code'] = ($testCode);
+        }
+
+        //$response = $request->execute();
+        $client = new Client();
+        $response = $client->post($this->baseApiUrl . '/' . $pixel_id . '/events', [
+            'json' => json_encode($request)
+        ])->getBody()->getContents();
 
         return $response;
 
